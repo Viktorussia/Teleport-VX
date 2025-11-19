882bbf8ae22bdd0484aa80b9d67fb101f76120f4
Viktorussia <nagornyiviktor084@gmail.com>
2025-11-20 06:41:46 +1000

    Create space.c
    Add space.c (space functions skeleton)

---
+++ vx-next/kernel/space.c
@@ -0,0 +1,21 @@
+#include "space.h"
+
+// Глобальный список пространств
+space_t *space_list_head = 0;
+
+// Создание нового Space
+space_t* space_create(space_type_t type) {
+    // Пока заглушка
+    return 0;
+}
+
+// Уничтожение Space
+void space_destroy(int space_id) {
+    // Заглушка
+}
+
+// Получение Space по ID
+space_t* space_get(int space_id) {
+    // Заглушка
+    return 0;
+}
