c3b9caa3f8c9ad6476be6d202320d57b0b7248e0
Viktorussia <nagornyiviktor084@gmail.com>
2025-11-22 15:29:36 +1000

    Create system_space.h
    Add system_space.h (system space API)

---
+++ vx-next/kernel/system_space.h
@@ -0,0 +1,12 @@
+#ifndef VX_SYSTEM_SPACE_H
+#define VX_SYSTEM_SPACE_H
+
+#include "space.h"
+
+// Инициализация системного пространства
+void system_space_init();
+
+// Получение системного пространства
+space_t* get_system_space();
+
+#endif
